12958e89b4e9c75ebee5a4a2fce7cc6d97f29098
oshram <oshra.maimon@cavsystems.com>
2021-01-26 18:33:45 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'My Shop';
+  msg = "";
+
+  buyItem(){
+    this.msg = (this.msg ? "" : "Thank you for your order!");
+  }
+}
