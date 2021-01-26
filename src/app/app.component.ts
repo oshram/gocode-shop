83504b572a3aca88be58c4f2d794ecb9d135e960
oshram <oshra.maimon@cavsystems.com>
2021-01-26 19:55:05 +0200

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Maimon Shop :)";
+  title = 'Maimon Shop :)';
   msg = "";
 
   buyItem(){
